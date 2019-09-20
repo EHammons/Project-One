9967a13a5dc2ed1d39fa12ee92783b7f53a9c88f
Kristen Stroup <kristenwhitson@gmail.com>
2019-09-20 10:44:09 -0500

    weather data to html

--- weather.js
+++ weather.js
@@ -1,8 +1,13 @@
 
 function displayWeather() {
 
-    var cityName = Austin // $("element").val().trim()
-    var queryUrl = "https://api.weatherbit.io/v2.0/forecast/3hourly?city=" + cityName + "&key=7169e5ccc01a4702a01e93ee6982101a&"
+    var cityName = "Austin" // $("element").val().trim()
+
+
+    var queryURL = "https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=7169e5ccc01a4702a01e93ee6982101a&units=I&days=5"
+   
+
+    // var queryUrl = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + cityName + "&key=7169e5ccc01a4702a01e93ee6982101a&units=I&days=5"
     
 
         $.ajax({
@@ -10,27 +15,32 @@ function displayWeather() {
             method: "GET"
         }).then(function (response) {
             var results = response.data
+            console.log(results);
 
-        for (var i = 0; i < results.length; i++) 
-            var weather = $("<div>")
-            console.log(austin)
+            for (var i = 0; i < results.length; i++) {
+                var cityWeather = $("<div>");
+                var temp = results[i].temp;
+                console.log(temp);
+                var weather = results[i].weather;
+                console.log(weather)
+                var wind = results[i].wind_spd;
+                console.log(wind);
+                
+                cityWeather.append(temp);
+                cityWeather.append(weather);
+                cityWeather.append(wind);
 
-            var cityWeather = 
+                $("#weatherDisplay").append(temp + "degrees");
+                $("#weatherDisplay").append(weather);
+                $("#weatherDisplay").append(wind + "mph");
+            }
 
 
-        }
+        
 
+            
 
-
-
-
-
-
-
-
-
-
-
+        });
 
 
 
@@ -38,3 +48,4 @@ function displayWeather() {
 
 
 }
+displayWeather ()
