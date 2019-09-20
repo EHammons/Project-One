cc3a0a4a28297437bf489591fdbe07a0bbf9b4c3
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-20 12:27:16 -0500

    Removed API info

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,24 +1,17 @@
 $("#search-button").on("click", function() {
-    var userInput = "city";
+    event.preventDefault();
+    var userInput = "Austin";
     console.log(userInput);
-    var googleQueryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + userInput + "&radius=1500&type=restaurant&keyword=cruise&key=" + googleAPIKey;
-    var googleAPIKey = "AIzaSyADAEzhWG-Zr1lJeCo5mJmk6Oh_JPIDjUI";
     $("#city-name").text(userInput);
 
-    $.ajax({
-        url: googleQueryURL,
-        method: "GET"
-    })
-        .then(function(response) {
-            console.log(response);
-            var dayWeather = response.day;
-            $("#weather-display").empty();
-            for (var i = 0; i < dayWeather.length; i++) {
-                var weatherDiv = $("<div>");
-                weatherDiv.addClass("daily");
-                var day = dayWeather[i];
-                weatherDiv.append(day);
-                $("#weather-display").append(weatherDiv);
-            }
-        })
+    var weatherDiv = $("<div>");
+    weatherDiv.addClass("daily");
+    var day = "2019-09-20";
+    console.log(day);
+    var convertedDate = moment(day, "YYYY-MM-DD").format("dddd");
+    console.log(convertedDate);
+    var temp = "80";
+    weatherDiv.append(convertedDate);
+    weatherDiv.append(temp);
+    $("#weather-display").append(weatherDiv);
 })
