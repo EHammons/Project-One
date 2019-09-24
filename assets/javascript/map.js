830c46809d51c26598ee7eda6cd35a34a959c96c
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-24 12:01:51 -0500

    Removed default controls from map after searching city

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -50,7 +50,9 @@ function displayCity(lat, lng) {
     var city = new google.maps.LatLng(cityLat, cityLng);
     map = new google.maps.Map(document.getElementById("map"), {
         center: city,
-        zoom: 14.5
+        zoom: 14.5,
+        disableDefaultUI: true,
+        zoomControl: true,
     });
 }
 
