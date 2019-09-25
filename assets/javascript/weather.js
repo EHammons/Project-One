a0dea69dde3f1daaf44c131f8a6273c8c4025e8b
Kristen Stroup <kristenwhitson@gmail.com>
2019-09-25 11:54:56 -0500

    add comments to weather.js

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -5,9 +5,10 @@ function displayWeather(cityName) {
 
     var cityName;
     if (cityName === undefined) {
+        //value from  the search bar set equal to city
         cityName = $("#search-bar").val().trim();
     }
-
+    //capitalize first letter of each city name
     function capitalizeFirstLetter(string) {
         var stringArr = string.split(" ");
         var result = "";
@@ -22,7 +23,7 @@ function displayWeather(cityName) {
     $("#city-name").text(cityName);
     console.log(cityName);
     
-                     
+       //  ajax  request using name or zip code              
     var queryUrl = "https://api.weatherbit.io/v2.0/forecast/daily?key=7169e5ccc01a4702a01e93ee6982101a&units=I&days=5&";
 
     if (isNaN(cityName)) {
@@ -44,7 +45,7 @@ function displayWeather(cityName) {
 
         var weatherDisplay = $("#weather-display");
             
-
+        //  loop through the weather results
         for (var i = 0; i < results.length; i++) {
             var dayWeather = $("<div>");
             var day = results[i].datetime;
@@ -53,23 +54,23 @@ function displayWeather(cityName) {
             dayWeather.addClass("day-weather");
             dayWeather.addClass("col");
 
-
+            //min and max temp results
             var maxTemp = results[i].max_temp;
             var minTemp = results[i].min_temp;
             
 
             var weatherIcon = results[i].weather.icon;
             
-
+            //append day of the week to browser
             var convertedDate = moment(day, "YYYY-MM-DD").format("dd");
             dayWeather.append(convertedDate);
 
-
+            //add weather icons to browswer
             var icon = $("<img>");
 
             icon.attr("src", "https://www.weatherbit.io/static/img/icons/" + weatherIcon + ".png")
             dayWeather.append(icon);
-
+            //round high & low temps to whole numbers
             dayWeather.append(Math.floor(maxTemp) + "° ");
             dayWeather.append(Math.floor(minTemp) + "° ");
 
@@ -80,7 +81,7 @@ function displayWeather(cityName) {
     });
 
 }
-
+//on click search function to display weather to browswer
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     displayWeather();
